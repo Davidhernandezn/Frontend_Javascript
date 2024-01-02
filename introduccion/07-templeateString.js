41fb22c95fbb39f83f76aabda23411e981b36381
Davidhernandezn <d4vidhernandezn@gmail.com>
2024-01-01 23:10:33 -0600

    Template Strings

---
+++ introduccion/07-templeateString.js
@@ -0,0 +1,8 @@
+//USAR VALORES
+const nombre = 'David Hernandez';
+const edad = 23;
+const pais = 'mexico';
+
+console.log("La persona es "+nombre+' Tiene: ',edad);
+
+console.log(`La persona es ${nombre} edad de ${edad}`);
